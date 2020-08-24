03c6b5eb43111c3d7b71acb964061933fe09aed5
SirBAO <107906890+SirBAO@users.noreply.github.com>
2020-08-24 19:12:04 +0000

    Fixed build errors

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <div id="App-Wrapper" :class="[appWrapperClass, theme]" :style="wrapperStyle">
+    <div
+      id="App-Container"
+      class="app-container max-w-10/12 lg:max-w-screen-2xl px-3 lg:px-8"
+      @keydown.meta.k.stop.prevent="handleOpenModal"
+      tabindex="-1"
+      :style="cssVariables"
+    >
+      <Heade
