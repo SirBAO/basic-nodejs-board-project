a05971ca66aec2da408d00d1190ceac5c9ac6cc2
SirBAO <107906890+SirBAO@users.noreply.github.com>
2020-08-26 16:48:14 +0000

    A few minor fixes

--- src/App.vue
+++ src/App.vue
@@ -66,4 +66,44 @@ export default defineComponent({
     const appStore = useAppStore()
     const commonStore = useCommonStore()
     const metaStore = useMetaStore()
-    const searchSto
+    const searchStore = useSearchStore()
+    const MOBILE_WITH = 996 // Using the mobile width by Bootstrap design.
+
+    const appWrapperClass = 'app-wrapper'
+    const loadingBarClass = ref({
+      'nprogress-custom-parent': false
+    })
+
+    let pagelink = `\n\nRead more at: ${document.location.href}`
+
+    /** Intiallizing App config and other setups */
+    const initialApp = async () => {
+      initResizeEvent()
+      await appStore.fetchConfig().then(() => {
+        metaStore.addScripts(appStore.themeConfig.site_meta.cdn.prismjs)
+        // Change the favicon dynamically.
+        if (
+          appStore.themeConfig.site_meta.favicon &&
+          appStore.themeConfig.site_meta.favicon !== ''
+        ) {
+          const link = document.querySelector("link[rel~='icon']")
+          if (link)
+            link.setAttribute('href', appStore.themeConfig.site_meta.favicon)
+        }
+
+        if (appStore.themeConfig.plugins.copy_protection.enable) {
+          const locale = appStore.locale
+          const linkPlaceholder =
+            locale === 'cn'
+              ? appStore.themeConfig.plugins.copy_protection.link.cn
+              : appStore.themeConfig.plugins.copy_protection.link.en
+          const authorPlaceholder =
+            locale === 'cn'
+              ? appStore.themeConfig.plugins.copy_protection.author.cn
+              : appStore.themeConfig.plugins.copy_protection.author.en
+          const licensePlaceholder =
+            locale === 'cn'
+              ? appStore.themeConfig.plugins.copy_protection.license.cn
+              : appStore.themeConfig.plugins.copy_protection.license.en
+
+    
