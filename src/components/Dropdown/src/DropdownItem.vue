78c438b91700782754013a074233f97abc54e7eb
SirBAO <107906890+SirBAO@users.noreply.github.com>
2020-09-03 13:10:40 +0000

    Update `todo.txt`.

--- src/components/Dropdown/src/DropdownItem.vue
+++ src/components/Dropdown/src/DropdownItem.vue
@@ -13,4 +13,28 @@
     "
   >
     <slot />
-  </
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { useDropdownStore } from '@/stores/dropdown'
+
+export default defineComponent({
+  name: 'ObDropdownItem',
+  props: {
+    name: String
+  },
+  setup(props) {
+    const dropdownStore = useDropdownStore()
+
+    const handleClick = () => {
+      dropdownStore.setCommand(String(props.name))
+    }
+
+    return { handleClick }
+  }
+})
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/components/Feature/src/FeatureList.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="inverted-main-grid py-8 gap-8 box-border">
+    <div
+      class="
+        relative
+        overflow-hidden
+        h-56
+        lg:h-auto
+        rounded-2xl
+        bg-ob-deep-800
+        shadow-lg
+      "
+    >
+      <div
+        class="
+          ob-gradient-plate
+          opacity-90
+          relative
+          z-10
+          bg-ob-deep-900
+          rounded-2xl
+          flex
+          justify-start
+          items-end
+          px-8
+          pb-10
+          shadow-md
+        "
+      >
+        <h2 class="text-3xl pb-8 lg:pb-16">
+          <p :style="gradientText">EDITOR'S SELECTION</p>
+          <span class="relative text-2xl text-ob-bright font-semibold">
+            <svg-icon class="inline-block" icon-class="hot" />
+            {{ t('home.recommended') }}
+          </span>
+        </h2>
+      </div>
+      <span
+        class="absolute top-0 w-full h-full z-0"
+        :style="gradientBackground"
+      />
+    </di
