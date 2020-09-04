13935aa156def3730cf25e28ccc986a6088348af
SirBAO <107906890+SirBAO@users.noreply.github.com>
2020-09-04 22:48:34 +0000

    Quick change

--- src/components/Header/src/Header.vue
+++ src/components/Header/src/Header.vue
@@ -6,4 +6,30 @@
       <Controls />
     </header>
   </div>
-</tem
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { Logo, Navigation, Controls } from '../index'
+
+export default defineComponent({
+  name: 'Header',
+  components: {
+    Logo,
+    Navigation,
+    Controls
+  },
+  props: {
+    msg: String
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.header-container {
+  .site-header {
+    max-width: var(--max-width);
+    @apply relative flex z-50 my-0 mx-auto py-4;
+  }
+}
+</style>
