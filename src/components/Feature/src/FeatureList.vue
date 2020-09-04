0e037860040b1e5e5abe11814f695dd7877ad6ce
SirBAO <107906890+SirBAO@users.noreply.github.com>
2020-09-04 21:40:25 +0000

    docs...

--- src/components/Feature/src/FeatureList.vue
+++ src/components/Feature/src/FeatureList.vue
@@ -39,4 +39,57 @@
         class="absolute top-0 w-full h-full z-0"
         :style="gradientBackground"
       />
-    </di
+    </div>
+
+    <ul class="grid lg:grid-cols-2 gap-8">
+      <template v-if="featurePosts.length > 0">
+        <li v-for="post in featurePosts" :key="post.slug">
+          <Article :data="post" />
+        </li>
+      </template>
+      <template v-else>
+        <li v-for="n in 2" :key="n">
+          <Article :data="{}" />
+        </li>
+      </template>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+import { useAppStore } from '@/stores/app'
+import { useI18n } from 'vue-i18n'
+import { computed, defineComponent, toRefs } from 'vue'
+import { Article } from '@/components/ArticleCard'
+
+export default defineComponent({
+  name: 'ObFeatureList',
+  components: {
+    Article
+  },
+  props: {
+    data: {
+      type: Array,
+      required: true
+    }
+  },
+  setup(props) {
+    const appStore = useAppStore()
+    const featurePosts = toRefs(props).data
+    const { t } = useI18n()
+
+    return {
+      gradientBackground: computed(() => {
+        return { background: appStore.themeConfig.theme.header_gradient_css }
+      }),
+      gradientText: computed(
+        () => appStore.themeConfig.theme.background_gradient_style
+      ),
+      featurePosts,
+      t
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/components/Header/src/Header.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="header-container">
+    <header class="site-header">
+      <Logo />
+      <Navigation />
+      <Controls />
+    </header>
+  </div>
+</tem
