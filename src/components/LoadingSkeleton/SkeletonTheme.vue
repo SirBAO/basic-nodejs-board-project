07ce378989cd6753fbbe0616cb53e0eac8727b9b
SirBAO <107906890+SirBAO@users.noreply.github.com>
2020-09-12 11:28:35 +0000

    Better testing

--- src/components/LoadingSkeleton/SkeletonTheme.vue
+++ src/components/LoadingSkeleton/SkeletonTheme.vue
@@ -29,3 +29,45 @@ export default defineComponent({
       type: String,
       default: DEFAULT_HIGHLIGHT
     },
+    duration: {
+      type: Number,
+      default: 1.5
+    },
+    tag: {
+      type: String,
+      default: 'div'
+    },
+    loading: Boolean
+  },
+  provide() {
+    return {
+      _themeStyle: this.themeStyle,
+      _skeletonTheme: this
+    }
+  },
+  setup() {
+    const themeStyle = { ...SkeletonStyle }
+    return { themeStyle }
+  },
+  render() {
+    const { color, highlight, duration } = this
+    this.themeStyle.backgroundColor = color
+    this.themeStyle.backgroundImage = `linear-gradient(
+      90deg,
+      ${color},
+      ${highlight},
+      ${color}
+    )`
+    if (duration) {
+      this.themeStyle.animation = `SkeletonLoading ${duration}s ease-in-out infinite`
+    } else {
+      this.themeStyle.animation = ''
+      this.themeStyle.backgroundImage = ''
+    }
+    if (this.tag) {
+      return h(this.tag, this.$slots.default)
+    }
+    return this.$slots.default
+  }
+})
+</script>
---
+++ src/components/MobileMenu.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="flex flex-col justify-center items-center">
+    <img
+      v-if="authorData.avatar !== ''"
+      class="diamond-avatar h-28 w-28 shadow-xl m-0"
+      :src="authorData.avatar || authorData.logo"
+      alt="avatar"
+    />
+    <ob-skeleton v-else width="7rem" height="7rem" circle />
+
+    <h2 class="text-center pt-4 text-4xl font-semibold text-ob-bright">
+      <template v-if="authorData.name">
+        {{ auth
