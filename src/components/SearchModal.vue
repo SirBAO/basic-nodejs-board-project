692c8cd16303020e1448052714276c7838ff2256
SirBAO <107906890+SirBAO@users.noreply.github.com>
2020-09-29 13:00:02 +0000

    update package version

--- src/components/SearchModal.vue
+++ src/components/SearchModal.vue
@@ -69,4 +69,46 @@
                   fill-rule="evenodd"
                   stroke-linecap="round"
                   stroke-linejoin="round"
-         
+                ></path>
+              </svg>
+            </button>
+          </form>
+        </header>
+        <div
+          id="Search-Dropdown"
+          class="search-dropdown"
+          v-if="
+            (searchResults.length > 0 || recentResults.length > 0) && !isEmpty
+          "
+        >
+          <div>
+            <section v-if="searchResults.length > 0">
+              <div class="search-hit-label">
+                {{ searchResultsCount }}
+              </div>
+              <ul id="search-menu">
+                <li
+                  v-for="(result, index) in searchResults"
+                  :key="result.slug"
+                  :class="{
+                    'search-hit': true,
+                    active: index == menuActiveIndex
+                  }"
+                  :id="'search-hit-item-' + index"
+                >
+                  <a href="javascript:void(0)" @click="handleLinkClick(result)">
+                    <div class="search-hit-container">
+                      <div class="search-hit-icon">
+                        <svg width="20" height="20" viewBox="0 0 20 20">
+                          <path
+                            d="M17 6v12c0 .52-.2 1-1 1H4c-.7 0-1-.33-1-1V2c0-.55.42-1 1-1h8l5 5zM14 8h-3.13c-.51 0-.87-.34-.87-.87V4"
+                            stroke="currentColor"
+                            fill="none"
+                            fill-rule="evenodd"
+                            stroke-linejoin="round"
+                          ></path>
+                        </svg>
+                      </div>
+                      <div class="search-hit-content-wrapper">
+                        <span
+            
