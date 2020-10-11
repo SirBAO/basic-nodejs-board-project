7c8812fe32ba1f9d10a612611ac9669032972410
SirBAO <107906890+SirBAO@users.noreply.github.com>
2020-10-11 13:47:13 +0000

    wireless: rt2x00: rt2800usb add more devices ids

--- src/components/SearchModal.vue
+++ src/components/SearchModal.vue
@@ -189,4 +189,31 @@
                             stroke-linejoin="round"
                           >
                             <path d="M18 3v4c0 2-2 4-4 4H2"></path>
-                     
+                            <path d="M8 17l-6-6 6-6"></path>
+                          </g>
+                        </svg>
+                      </div>
+                    </div>
+                  </a>
+                </li>
+              </ul>
+            </section>
+          </div>
+        </div>
+        <div class="search-startscreen" v-else-if="!isEmpty">
+          <p>{{ t('settings.no-recent-search') }}</p>
+        </div>
+        <div class="search-startscreen" v-else>
+          <p>{{ t('settings.no-search-result') }}</p>
+        </div>
+        <div class="search-footer">
+          <div class="search-logo">
+            <a
+              href="https://www.algolia.com/docsearch"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <span class="search-label">{{ t('settings.searched-by') }}</span>
+              <!-- <svg width="77" height="19" style="color: #5468ff">
+              <path
+                d="M2.5067 0h14.0245c1.384.001 2.5058 1.1205 2.5068 2.5017V16.5c-.0014 1.3808-1.1232 2.4995-2.5068 2.5H2.5067C1.1232 18.9995.0014 17.8808 0 16.5V2.4958A2.495 2.495 0 01.735.7294 2.505 2.505 0 012.5068 0zM37.95 15.0695c-3.7068.0168-3.7068-2.986-3.7068-3.4634L34.2372.3576 36.498 0v11.1794c0 .2715 0 1.9889 1.452 1.994v1.8961zm-9.1666-1.8388c.694 0 1.2086-.0397 1.5678-.1088v-2.2934a5.3639 5.3639 0 00-1.3303-.1679 4.8283 4.8283 0 00-.758.0582 2.2845 2.2845 0 00-.688.2024c-.2029.0979-.371.2362-.4919.4142-.1268.1788-.185.2826-.185.5533 0 .5297.185.8359.5205 1.0375.3355.2016.7928.3053 1.365.3053v-.0008zm-.1969-8.1817c.7463 0 1.3768.092 1.88
