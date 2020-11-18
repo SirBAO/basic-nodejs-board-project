be1cba2430cd44b33221439201bf9529272cb289
SirBAO <107906890+SirBAO@users.noreply.github.com>
2020-11-18 17:18:47 +0000

    empty log message

---
+++ src/components/SvgIcon/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div
+    v-if="isExternalClass"
+    :style="styleExternalIcon"
+    class="svg-external-icon svg-icon"
+    v-bind="$attrs"
+  />
+  <svg v-else :class="svgClass" aria-hidden="true" v-bind="$attrs">
+    <use :href="iconName" />
+  </svg>
+</template>
+
+<script lang="ts">
+import { defineComponent, computed } from 'vue'
+// doc: https://panjiachen.github.io/vue-element-admin-site/feature/component/svg-icon.html#usage
+import { isExternalIcon } from '@/utils/validate'
+
+export default defineComponent({
+  nam
