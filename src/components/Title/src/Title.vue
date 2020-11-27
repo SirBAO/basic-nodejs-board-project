73eb4a695d3d13438542f51126a8f8f143cb2756
SirBAO <107906890+SirBAO@users.noreply.github.com>
2020-11-27 09:08:12 +0000

    E_STRICT

---
+++ src/components/Title/src/Title.vue
@@ -0,0 +1,13 @@
+<template>
+  <p
+    :id="id"
+    class="
+      relative
+      opacity-90
+      flex
+      items-center
+      pt-12
+      pb-2
+      mb-8
+      text-3xl text-ob-bright
+      upperca
