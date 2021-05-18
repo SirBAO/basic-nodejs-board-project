3b46f485c825d3ca657dc566f427c738de282871
SirBAO <107906890+SirBAO@users.noreply.github.com>
2021-05-18 11:05:13 +0000

    remove debugging print

--- src/stores/navigator.ts
+++ src/stores/navigator.ts
@@ -20,3 +20,48 @@ export const useNavigatorStore = defineStore({
         if (this.openMenu === false) {
           scrollPosition = window.pageYOffset
           bodyEl.style.overflow = 'hidden'
+          bodyEl.style.position = 'fixed'
+          bodyEl.style.top = `-${scrollPosition}px`
+          bodyEl.style.width = '100%'
+
+          appEl.style.overflow = 'hidden'
+          appEl.style.maxHeight = '100vh'
+          appWrapperEl.style.borderRadius = '16px'
+          appWrapperEl.style.overflow = 'hidden'
+          appWrapperEl.style.maxHeight = '100vh'
+          appWrapperEl.style.minHeight = '100vh'
+          appWrapperEl.style.transform =
+            'translate3d(302px, 0px, 0px) scale3d(0.86, 0.86, 1)'
+          setTimeout(() => {
+            appMobileEl.style.opacity = '1'
+            appMobileEl.style.transform = 'translateY(0)'
+          }, 200)
+          this.openMenu = true
+        } else {
+          bodyEl.style.removeProperty('overflow')
+          bodyEl.style.removeProperty('position')
+          bodyEl.style.removeProperty('top')
+          bodyEl.style.removeProperty('width')
+          window.scrollTo(0, scrollPosition)
+
+          appMobileEl.style.opacity = '0'
+          appMobileEl.style.transform = 'translateY(-20%)'
+
+          appWrapperEl.style.transform =
+            'translate3d(0px, 0px, 0px) scale3d(1, 1, 1)'
+          appWrapperEl.style.borderRadius = '0'
+
+          setTimeout(() => {
+            appEl.style.overflow = 'auto'
+            appEl.style.maxHeight = 'initial'
+            appWrapperEl.style.overflow = 'auto'
+            appWrapperEl.style.maxHeight = 'initial'
+            appWrapperEl.style.minHeight = 'initial'
+            appWrapperEl.style.transform = 'none'
+            this.openMenu = false
+          }, 376)
+        }
+      }
+    },
+    toggleOpenNavigator() {
+  
